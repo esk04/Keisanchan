63d2b1bc647ed7221aa3a2d0115ae229a69b5755
esk04 <kokuu04@gmail.com>
2018-08-17 11:27:39 +0900

    DAO追加

--- Keisanchan/src/com/internousdev/keisanchan/util/DBConnector.java
+++ Keisanchan/src/com/internousdev/keisanchan/util/DBConnector.java
@@ -1,30 +1,32 @@
 package com.internousdev.keisanchan.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DBConnector {
-	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/ecsite";
 
-	private static String user = "root";
-	private static String password = "mysql";
+private static String driverName = "com.mysql.jdbc.Driver";
+private static String url = "jdbc:mysql://localhost/ecsite";
+private static String user = "root";
+private static String password = "mysql";
+public Connection getConnection() {
+Connection con = null;
+try {
 
-		public Connection getConnection(){
-			Connection con = null;
+Class.forName(driverName);
+con = (Connection) DriverManager.getConnection(url,user,password);
 
-			try{
-				Class.forName(driverName);
-				con = (Connection) DriverManager.getConnection(url,user,password);
-			}catch(ClassNotFoundException e){
-				e.printStackTrace();
 
-			}catch(SQLException e){
-				e.printStackTrace();
-			}
 
-			return  con;
-		}
+} catch(ClassNotFoundException e) {
+
+e.printStackTrace();
+
+} catch(SQLException e) {
+
+e.printStackTrace();
+
+}
+return con;
+}
 
 }
